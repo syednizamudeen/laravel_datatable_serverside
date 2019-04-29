a270ad1b0e0745942c8ceee87f3e907386c6ed12
Syed Nizamudeen <syednizamudeen@users.noreply.github.com>
2019-04-29 23:12:08 +0800

    Temporarily Load plugins from CDN

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,19 +3,48 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('vendor/bootstrap/dist/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('vendor/datatables/css/dataTables.bootstrap4.min.css')}}">
+    {{-- <link rel="stylesheet" href="http://cdn.datatables.net/buttons/1.5.6/css/buttons.dataTables.css"> --}}
+    <link rel="stylesheet" href="http://cdn.datatables.net/buttons/1.5.6/css/buttons.bootstrap4.min.css">
     <link rel="stylesheet" href="{{asset('vendor/fontawesome/css/all.css')}}" type="text/css">
+    <style>
+        .dt-button-collection li.buttons-columnVisibility a:before {
+          display: inline-block;
+          font: normal normal normal 14px/1 FontAwesome;
+          font-size: inherit;
+          text-rendering: auto;
+          -webkit-font-smoothing: antialiased;
+          transform: translate(0,0);
+          content: "\f00d";
+          margin-right: 13px;
+        }
+    
+        .dt-button-collection li.buttons-columnVisibility.active a:before {
+          display: inline-block;
+          font: normal normal normal 14px/1 FontAwesome;
+          font-size: inherit;
+          text-rendering: auto;
+          -webkit-font-smoothing: antialiased;
+          transform: translate(0,0);
+          content: "\f00c";
+          margin-right: 10px;
+        }
+        .card-header > .datatable-box-tools {
+          color: #ffffff;
+          position: absolute;
+          right: 10px;
+          top: 5px;
+        }
+    </style>
     <title>{{config('app.name','Laravel DataTable')}}</title>
 </head>
 <body>
     <main role="main" class="flex-shrink-0">
         <div class="container-fluid">
             <h1 class="mt-5">{{$title}}</h1>
-            <h2 class="my-4">{{$heading}}</h2>
             <div class="table-responsive">
-                <table id="example" class="table table-striped table-bordered" style="width:100%">
+                <table id="example" class="table table-striped" style="width:100%; margin-top: unset; margin-bottom: unset;">
                     <thead>
                         <tr>
                             @foreach($columnmapping as $column)
@@ -42,18 +71,38 @@
     <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
     <script src="{{asset('vendor/datatables/js/jquery.dataTables.min.js')}}"></script>
     <script src="{{asset('vendor/datatables/js/dataTables.bootstrap4.min.js')}}"></script>
+    <script src="http://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
+    <script src="http://cdn.datatables.net/buttons/1.5.6/js/buttons.bootstrap4.min.js"></script>
+    <script src="http://cdn.datatables.net/buttons/1.5.6/js/buttons.colVis.js"></script>
     <script src="{{asset('vendor/bootstrap/dist/js/bootstrap.min.js')}}"></script>
     <script src="{{asset('vendor/moment/moment.min.js')}}"></script>
     <script>
     var APP_URL = {!! json_encode(url('/')) !!}, dataTable, columns = {!! $columns !!}, name = {!! array_search('name', array_column(json_decode($columns, true), 'data')) !!}, income = {!! array_search('annualincome', array_column(json_decode($columns, true), 'data')) !!}, age = {!! array_search('age', array_column(json_decode($columns, true), 'data')) !!}, created_at = {!! array_search('created_at', array_column(json_decode($columns, true), 'data')) !!}, updated_at = {!! array_search('updated_at', array_column(json_decode($columns, true), 'data')) !!};
     $(document).ready(function() {
         dataTable = $('#example').DataTable({
+            "dom": '<"card"<"card-header"<"boxtitle float-left"><"float-right"B>><"card-body p-0"t><"card-footer"<"row"<"col-6"i><"col-6"p>>>>',
             "processing": true,
             "serverSide": true,
             "orderCellsTop": true,
             "ajax": APP_URL+"/home/datatable",
             "columns": columns,
             "order": [[ name, "asc" ]],
+            "buttons": [
+                {
+                    extend:    'colvis',
+                    text:      '<i class="fa fa-columns fa-lg fa-fw text-primary"></i>Columns',
+                    titleAttr: 'Show/Hide Column(s)',
+                    className: 'btn btn-sm',
+                    autoClose: true,
+                    columns: ':not(.disableColhide)',
+                },
+                {
+                    extend:    'pageLength',
+                    titleAttr: 'Rows per Page',
+                    className: 'btn btn-primary btn-sm',
+                    autoClose: true,
+                }
+            ],
             "columnDefs": [
                 {
                     "targets": income,
@@ -72,6 +121,7 @@
                 }
             ]
         });
+        $("div.boxtitle").html('<h3 class="box-title">{{$heading}}</h3>');
         $.each($('.input-filter', '#example tr'), function(i) {
             var column = dataTable.columns($(this).index());
             $('input', this).on('keyup change', function(e) {
