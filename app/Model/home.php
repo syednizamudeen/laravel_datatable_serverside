4f197eebba4be4141004241d91dca0db0837146f
Syed Nizamudeen <syednizamudeen@users.noreply.github.com>
2019-05-03 15:16:03 +0800

    Column Type Updated

--- app/Model/home.php
+++ app/Model/home.php
@@ -7,13 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class home extends Model
 {
     static $dataTableColumnMapping = [
-        ['database'=>'id', 'label'=> '#' , 'type'=>'text'],
+        ['database'=>'id', 'label'=> '#' , 'type'=>'checkbox'],
         ['database'=>'name', 'label'=> 'Name' , 'type'=>'text'],
         ['database'=>'address', 'label'=> 'Address' , 'type'=>'text'],
         ['database'=>'contactno', 'label'=> 'Contact No' , 'type'=>'text'],
         ['database'=>'annualincome', 'label'=> 'Income' , 'type'=>'text'],
         ['database'=>'age', 'label'=> 'Age' , 'type'=>'text'],
-        ['database'=>'created_at', 'label'=> 'Created At' , 'type'=>'text'],
-        ['database'=>'updated_at', 'label'=> 'Updated At' , 'type'=>'text']
+        ['database'=>'created_at', 'label'=> 'Created At' , 'type'=>'datetimerange'],
+        ['database'=>'updated_at', 'label'=> 'Updated At' , 'type'=>'datetimerange']
     ];
 }
