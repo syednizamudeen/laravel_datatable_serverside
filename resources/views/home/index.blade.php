4a0e023fc2cd8988b55e9a95cbbdeec4b739ef30
Syed Nizamudeen <syednizamudeen@users.noreply.github.com>
2019-05-03 15:14:32 +0800

    Added CDN for testing
    Font Awesome & Awesome Bootstrap Checkbox

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/3.0.5/daterangepicker.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/awesome-bootstrap-checkbox/1.0.1/awesome-bootstrap-checkbox.min.css">
     <style>
         .dt-button-collection li.buttons-columnVisibility a:before {
           display: inline-block;
@@ -32,6 +35,12 @@
           right: 10px;
           top: 5px;
         }
+        /* $font-family-icon: 'Font Awesome 5 Free'; */
+        .abc-checkbox input[type="checkbox"]:checked + label::after, .abc-checkbox input[type="radio"]:checked + label::after {
+            font-family: "Font Awesome 5 Free";
+            content: "\f00c"; 
+            font-weight: 900;
+        }
     </style>
     <title>{{config('app.name','Laravel DataTable')}}</title>
 </head>
@@ -50,12 +59,19 @@
                         <tr>
                             @foreach($columnmapping as $column)
                             <th class="input-filter">
+                                @if($column['type'] == 'checkbox')
+                                <div class="form-check abc-checkbox abc-checkbox-primary">
+                                    <input class="form-check-input" id="checkbox1" type="checkbox">
+                                    <label class="form-check-label" for="checkbox1"></label>
+                                </div>
+                                @else
                                 <div class="input-group input-group-sm">
-                                    <input type="{{$column['type']}}" class="form-control" placeholder="Search {{$column['label']}}">
+                                    <input type="text" class="form-control" placeholder="Search {{$column['label']}}" filter-type="{{$column['type']}}">
                                     <div class="input-group-append">
                                         <button class="btn btn-outline-secondary clearfilter" type="button"><i class="fas fa-times-circle"></i></button>
                                     </div>
                                 </div>
+                                @endif
                             </th>
                             @endforeach
                         </tr>
@@ -65,8 +81,9 @@
         </div>
     </main>
     <script src="{{asset('js/app.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/3.0.5/daterangepicker.min.js"></script>
     <script>
-    var APP_URL = {!! json_encode(url('/')) !!}, dataTable, columns = {!! $columns !!}, name = {!! array_search('name', array_column(json_decode($columns, true), 'data')) !!}, income = {!! array_search('annualincome', array_column(json_decode($columns, true), 'data')) !!}, age = {!! array_search('age', array_column(json_decode($columns, true), 'data')) !!}, created_at = {!! array_search('created_at', array_column(json_decode($columns, true), 'data')) !!}, updated_at = {!! array_search('updated_at', array_column(json_decode($columns, true), 'data')) !!};
+    var APP_URL = {!! json_encode(url('/')) !!}, dataTable, columns = {!! $columns !!}, id = {!! array_search('id', array_column(json_decode($columns, true), 'data')) !!}, name = {!! array_search('name', array_column(json_decode($columns, true), 'data')) !!}, income = {!! array_search('annualincome', array_column(json_decode($columns, true), 'data')) !!}, age = {!! array_search('age', array_column(json_decode($columns, true), 'data')) !!}, created_at = {!! array_search('created_at', array_column(json_decode($columns, true), 'data')) !!}, updated_at = {!! array_search('updated_at', array_column(json_decode($columns, true), 'data')) !!};
     $(document).ready(function() {
         dataTable = $('#example').DataTable({
             "dom": '<"card"<"card-header"<"boxtitle float-left"><"float-right"B>><"card-body p-0"t><"card-footer"<"float-left"i><"float-right"p>>>',
@@ -74,6 +91,13 @@
             "serverSide": true,
             "orderCellsTop": true,
             "ajax": APP_URL+"/home/datatable",
+            "ajax": {
+                "url": APP_URL+"/home/datatable",
+                "data": function( d ) {
+                    d.myKey = "myValue";
+                    // d.custom = $('#myInput').val();
+                }
+            },
             "columns": columns,
             "order": [[ name, "asc" ]],
             "lengthMenu": [
@@ -112,6 +136,14 @@
             },
             "columnDefs": [
                 {
+                    "targets": id,
+                    "searchable": false,
+                    "orderable": false,
+                    "render": function ( data, type, row, meta ) {
+                        return data;
+                    }
+                },
+                {
                     "targets": income,
                     "className": 'text-right',
                     "render": $.fn.dataTable.render.number( ',', '.', 2, '$' )
@@ -144,6 +176,22 @@
         $("div.boxtitle").html('<h4 class="m-0">{{$heading}}</h4>');
         $.each($('.input-filter', '#example tr'), function(i) {
             var column = dataTable.columns($(this).index());
+            if ($('input', this).attr('filter-type') == 'datetimerange')
+            {
+                $('input', this).daterangepicker({
+                    autoUpdateInput: false,
+                    timePicker: true,
+                    startDate: moment().startOf('hour'),
+                    endDate: moment().startOf('hour').add(32, 'hour'),
+                    locale: {
+                        format: 'M/DD hh:mm A'
+                    }
+                }).on('apply.daterangepicker', function(ev, picker) {
+                    $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+                }).on('cancel.daterangepicker', function(ev, picker) {
+                    $(this).val('');
+                });
+            }
             $('input', this).on('keyup change', function(e) {
                 e.preventDefault();
                 // if ( dataTable.column(i).search() !== this.value ) {
@@ -153,8 +201,8 @@
             });
             $('.clearfilter', this).on('click', function(e) {
                 e.preventDefault();
-                $(this).parent().parent().find("input[type=text]:first").val("");
-                dataTable.column(i).search( '').draw();
+                $(this).parent().parent().find("input[type=text]:first").val('');
+                dataTable.column(i).search('').draw();
             });
         });
     });
